d34bde33434a54d1264b28c206a795ad4eda16c2
Tristan Meinsma <tristan.meinsma@gmail.com>
2023-07-29 10:42:57 +0200

    working crud song endpoints, tested with postman

--- src/main/java/lumberjxck/portfolio/website/backend/BackendApplication.java
+++ src/main/java/lumberjxck/portfolio/website/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package lumberjxck.portfolio.website.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secret.properties")
+@PropertySource("classpath:application.properties")
 public class BackendApplication {
 
 	public static void main(String[] args) {
